92d658329a7f9a30b39e6c0d617fdc7d3af7618d
Jess Merry <merryja.email@gmail.com>
2022-04-25 19:35:39 -0400

    word count

--- word_frequency.py
+++ word_frequency.py
@@ -6,14 +6,15 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
-
-def print_word_freq(file):
 # """- remove punctuation"""
 # """- normalize all words to lowercase"""
 # """- remove "stop words" -- words used so frequently they are ignored"""
 # """- go through the file word by word and keep a count of how often each word is used""" (use .count)
 
-    with open(file,'r') as file_contents:
+
+def print_word_freq(file):
+
+    with open(file, 'r') as file_contents:
         contents_string = file_contents.read()
         contents_lower = contents_string.lower()
         for character in string.punctuation:
@@ -23,15 +24,11 @@ def print_word_freq(file):
         for word in contents_split:
             if word in STOP_WORDS:
                 contents_split.remove(word)
-            if word in contents_split:
-                contents_split.count(word)
 
-#        contents_count = contents_split.count(word)
-#        for word_count in contents_count:
-#            if word_count in contents_split:
-#                contents_split.count(word)
-
-    print(contents_split)
+            else:
+                count = contents_split.count(word)
+                ast = "*" * count
+                print("word: {} | count: {} {}".format(word, count, ast))
 
 
 if __name__ == "__main__":
